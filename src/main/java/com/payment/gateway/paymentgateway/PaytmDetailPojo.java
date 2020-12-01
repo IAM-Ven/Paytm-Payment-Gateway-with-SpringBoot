1e5b9277e8903b010ff6a18e69b45f82fe0d09bf
sumant1122 <sumant1122@gmail.com>
2020-12-01 20:46:04 +0530

    Initial Commit

---
+++ src/main/java/com/payment/gateway/paymentgateway/PaytmDetailPojo.java
@@ -0,0 +1,25 @@
+package com.payment.gateway.paymentgateway;
+
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@ConfigurationProperties("paytm.payment.sandbox")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaytmDetailPojo {
+    private String merchantId;
+	private String merchantKey;
+	private String channelId;
+	private String website;
+	private String industryTypeId;
+	private String paytmUrl;
+	private Map<String, String> details;
+}
---
+++ src/test/java/com/payment/gateway/paymentgateway/PaymentgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.gateway.paymentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
